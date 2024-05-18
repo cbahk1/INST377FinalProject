6e227f34da39b6e08adfdb7ce8f6e794ded36cb0
Luckystar Helil <Lhelil@terpmail.umd.edu>
2024-05-18 13:13:05 -0400

    modifing js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-document.getElementById('searchButton').addEventListener('click', function() {
-    const query = document.getElementById('searchInput').value;
-    searchMovies(query);
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('searchButton').addEventListener('click', function () {
+        const query = document.getElementById('searchInput').value;
+        searchMovies(query);
+    });
 });
 
 function searchMovies(query) {
-    const apiKey = '5914722f'; 
+    const apiKey = '5914722f';
     const url = `https://www.omdbapi.com/?s=${query}&apikey=${apiKey}`;
-    
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -19,7 +21,23 @@ function searchMovies(query) {
         .catch(error => console.error('Error fetching data:', error));
 }
 
+function displayMovies(movies) {
+    const movieList = document.getElementById('movieList');
+    movieList.innerHTML = '';
 
-function redirect() {
-    window.location.href = "about.html";
+    movies.forEach(movie => {
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('movie');
+
+        movieElement.innerHTML = `
+            <img src="${movie.Poster}" alt="${movie.Title}">
+            <div class="movie-details">
+                <h2>${movie.Title}</h2>
+                <p>${movie.Year}</p>
+                <p>${movie.Type}</p>
+            </div>
+        `;
+
+        movieList.appendChild(movieElement);
+    });
 }
